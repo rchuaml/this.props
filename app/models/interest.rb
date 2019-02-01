72e1f44aa26a5baa6164155d0bf555978671342e
hongchiong <hongchiong@gmail.com>
2019-02-01 15:30:05 +0800

    Figured out query statement. Save as comment in interests model

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -2,3 +2,8 @@ class Interest < ApplicationRecord
   belongs_to :user
   belongs_to :house
 end
+
+
+# Query the houses that a certain user is interested in:
+
+# House.where(id: Interest.where(user_id: certainuser.id))
