d578be6ba9ee9ede5b3cc914ce7267374d8da82a
hongchiong <hongchiong@gmail.com>
2019-02-02 14:28:25 +0800

    plot houses markers

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@
 House.create(name: 'Atrium', location: 'Orchard', lat: 1.306497, long: 103.831235, price: 680000, bedrooms: 3, bathrooms: 2, floor_area: 88, furnishing: true, floor_levels: 22, lease_left: 95, user_id: 2)
 House.create(name: 'Sentosa Cove', location: 'Sentosa', lat: 1.246248, long: 103.839700, price: 680000, bedrooms: 3, bathrooms: 2, floor_area: 88, furnishing: true, floor_levels: 22, lease_left: 95, user_id: 2)
 House.create(name: 'Champions Court', location: 'Woodlands', lat: 1.434314, long: 103.788832, price: 680000, bedrooms: 3, bathrooms: 2, floor_area: 88, furnishing: true, floor_levels: 22, lease_left: 95, user_id: 2)
+House.create(name: 'Blk16', location: 'Woodlands', lat: 1.424314, long: 103.768832, price: 680000, bedrooms: 3, bathrooms: 2, floor_area: 88, furnishing: true, floor_levels: 22, lease_left: 95, user_id: 1)
